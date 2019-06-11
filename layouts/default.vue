9f61d893c68cf3bbffce0e96258ad2ff69a99735
Lukas Juhas <itslukasjuhas@gmail.com>
2019-06-11 22:58:29 +0100

    initial setup

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-w-full layout">
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  min-width: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col content-center min-w-full">
+    <div class="container mx-auto p-4">
+      <h1 class="text-4xl font-bold">
+        Spotify It
+      </h1>
+      <h2 class="subtitle">
+        Spotify my words into playlist.
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+          >GitHub</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>

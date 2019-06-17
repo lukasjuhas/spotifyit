00712109c15b2ddfd5d1a4eb032f43664e99119e
Lukas Juhas <itslukasjuhas@gmail.com>
2019-06-17 15:10:04 +0100

    Spotify redirect url as env variable

--- store/index.js
+++ store/index.js
@@ -81,7 +81,7 @@ export const actions = {
       response_type: 'token',
       client_id: process.env.SPOTIFY_CLIENT_ID,
       scope: 'playlist-modify-private playlist-modify-public',
-      redirect_uri: 'http://localhost:3000/search/',
+      redirect_uri: process.env.SPOTIFY_REDIRECT_URL,
     };
 
     const urlParams = Object.entries(params).map(([key, val]) => `${key}=${val}`).join('&');
